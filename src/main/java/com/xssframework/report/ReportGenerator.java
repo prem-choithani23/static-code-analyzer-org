e279b3f60c38584d93ac0e62f87d32222d421308
prem-choithani23 <premchoithani4@gmail.com>
2026-04-27 19:46:12 +0530

    feat: add report generator contract

---
+++ src/main/java/com/xssframework/report/ReportGenerator.java
@@ -0,0 +1,25 @@
+package com.xssframework.report;
+
+import com.xssframework.model.Finding;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ * Contract for report generators.
+ *
+ * A report generator transforms a list of findings into a human-readable report
+ * (HTML, JSON, etc.) and writes it to disk.
+ */
+public interface ReportGenerator {
+
+    /**
+     * Generate a report from the given findings and write it to the specified path.
+     *
+     * @param findings      list of vulnerabilities to report
+     * @param outputPath    where to write the report file
+     * @throws IOException  if file writing fails
+     */
+    void generate(List<Finding> findings, Path outputPath) throws IOException;
+}
